eab678dbf5711a11e00759d2b00bb7d93063615a
Yanghg <yanghong2014@u.northwestern.edu>
2015-05-02 21:51:57 -0500

    ddd

--- test.py
+++ test.py
@@ -4,4 +4,6 @@ execfile("SudokuStarter.py")
 sb = init_board("input_puzzles/easy/16_16.sudoku")
 #sb = init_board("input_puzzles/easy/25_25.sudoku")
 sb.print_board()
+
 fb = solve(sb, False, False, True, False)
+
